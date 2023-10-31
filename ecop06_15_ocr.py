6ac7a115337eefb1bfcb0e7c381588f6c2fab0fb
CarlaoStudent100 <148779926+CarlaoStudent100@users.noreply.github.com>
2023-10-31 17:09:43 -0300

    Update ecop06_15_ocr.py

--- ecop06_15_ocr.py
+++ ecop06_15_ocr.py
@@ -1,1 +1,10 @@
-
+payload = {'isOverlayRequired': overlay,
+           'apikey': api_key,
+           'language': language,
+           }
+with open(filename, 'rb') as f:
+    r = requests.post('https://api.ocr.space/parse/image',
+                      files={filename: f},
+                      data=payload,
+                      )
+return r.content.decode()
